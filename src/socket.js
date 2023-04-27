d99d48fab126ded5d0a13f3dce567a72ee43352d
arunadmap <49647193+arunadmap@users.noreply.github.com>
2023-04-28 04:40:56 +0530

    Update socket.js

--- src/socket.js
+++ src/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = process.env.NODE_ENV === 'production' ? undefined : 'http://localhost:3030';
+const URL = process.env.NODE_ENV === 'production' ? undefined : 'https://4684-112-134-210-105.ngrok-free.app';
 
 export const socket = io(URL);
