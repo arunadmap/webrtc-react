2bbfc158c987f3c3ece71233bf0f368ed65999f3
Aruna <arunadmap@icloud.com>
2023-04-30 15:23:41 +0530

    https://github.com/arunadmap/webrtc-react

--- src/pages/home/webrtc/webrtc.jsx
+++ src/pages/home/webrtc/webrtc.jsx
@@ -1,49 +1,157 @@
-import React, { useEffect } from "react";
-import JoinMeeting from "./JoinMeeting";
-import EndMeeting from "./EndMeeting";
-import { Card } from "antd";
-
-export default function WebRTC({
-    isConnected,
-    onJoinMeetingClick,
-    onEndMeetingClick,
-    ...props
-}) {
+import { useParams } from "react-router-dom";
+import { useRef, useEffect } from "react";
+import socketio from "socket.io-client";
 
 
-    useEffect(() => {
-        if (isConnected) {
-            playVideoFromCamera()
-        }
-    }, [isConnected])
+function CallScreen() {
+  const params = useParams();
+  const localUsername = params.username;
+  const roomName = params.room;
+  const localVideoRef = useRef(null);
+  const remoteVideoRef = useRef(null);
 
-    async function playVideoFromCamera() {
-        try {
-            const constraints = { 'video': true, 'audio': false };
-            const stream = await navigator.mediaDevices.getUserMedia(constraints);
-            const videoElement = document.querySelector('video#localVideo');
-            videoElement.srcObject = stream;
-        } catch (error) {
-            console.error('Error opening video camera.', error);
-        }
+  const socket = socketio("https://signaling-server-flask.herokuapp.com/", {
+    autoConnect: false,
+  });
+
+  let pc; // For RTCPeerConnection Object
+
+  const sendData = (data) => {
+    socket.emit("data", {
+      username: localUsername,
+      room: roomName,
+      data: data,
+    });
+  };
+
+  const startConnection = () => {
+    navigator.mediaDevices
+      .getUserMedia({
+        audio: false,
+        video: true,
+      })
+      .then((stream) => {
+        console.log("Local Stream found");
+        localVideoRef.current.srcObject = stream;
+        socket.connect();
+        socket.emit("join", { username: localUsername, room: roomName });
+      })
+      .catch((error) => {
+        console.error("Stream not found: ", error);
+      });
+  };
+
+  const onIceCandidate = (event) => {
+    if (event.candidate) {
+      console.log("Sending ICE candidate");
+      sendData({
+        type: "candidate",
+        candidate: event.candidate,
+      });
     }
+  };
 
+  const onTrack = (event) => {
+    console.log("Adding remote track");
+    remoteVideoRef.current.srcObject = event.streams[0];
+  };
 
-    return (
-        <>
-            {isConnected ? (
-                <>
-                    <Card
-                        actions={[
-                            <EndMeeting onEndMeetingClick={onEndMeetingClick}></EndMeeting>,
-                        ]}
-                    >
-                        <video id="localVideo" autoplay playsinline controls="false" width={400}/>
-                    </Card>
-                </>
-            ) : (
-                <JoinMeeting onJoinMeetingClick={onJoinMeetingClick}></JoinMeeting>
-            )}
-        </>
-    );
+  const createPeerConnection = () => {
+    try {
+      pc = new RTCPeerConnection({
+        iceServers: [
+          {
+            urls: "stun:openrelay.metered.ca:80",
+          },
+          {
+            urls: "turn:openrelay.metered.ca:80",
+            username: "openrelayproject",
+            credential: "openrelayproject",
+          },
+          {
+            urls: "turn:openrelay.metered.ca:443",
+            username: "openrelayproject",
+            credential: "openrelayproject",
+          },
+          {
+            urls: "turn:openrelay.metered.ca:443?transport=tcp",
+            username: "openrelayproject",
+            credential: "openrelayproject",
+          },
+        ],
+      });
+      pc.onicecandidate = onIceCandidate;
+      pc.ontrack = onTrack;
+      const localStream = localVideoRef.current.srcObject;
+      for (const track of localStream.getTracks()) {
+        pc.addTrack(track, localStream);
+      }
+      console.log("PeerConnection created");
+    } catch (error) {
+      console.error("PeerConnection failed: ", error);
+    }
+  };
+
+  const setAndSendLocalDescription = (sessionDescription) => {
+    pc.setLocalDescription(sessionDescription);
+    console.log("Local description set");
+    sendData(sessionDescription);
+  };
+
+  const sendOffer = () => {
+    console.log("Sending offer");
+    pc.createOffer().then(setAndSendLocalDescription, (error) => {
+      console.error("Send offer failed: ", error);
+    });
+  };
+
+  const sendAnswer = () => {
+    console.log("Sending answer");
+    pc.createAnswer().then(setAndSendLocalDescription, (error) => {
+      console.error("Send answer failed: ", error);
+    });
+  };
+
+  const signalingDataHandler = (data) => {
+    if (data.type === "offer") {
+      createPeerConnection();
+      pc.setRemoteDescription(new RTCSessionDescription(data));
+      sendAnswer();
+    } else if (data.type === "answer") {
+      pc.setRemoteDescription(new RTCSessionDescription(data));
+    } else if (data.type === "candidate") {
+      pc.addIceCandidate(new RTCIceCandidate(data.candidate));
+    } else {
+      console.log("Unknown Data");
+    }
+  };
+
+  socket.on("ready", () => {
+    console.log("Ready to Connect!");
+    createPeerConnection();
+    sendOffer();
+  });
+
+  socket.on("data", (data) => {
+    console.log("Data received: ", data);
+    signalingDataHandler(data);
+  });
+
+  useEffect(() => {
+    startConnection();
+    return function cleanup() {
+      pc?.close();
+    };
+  }, []);
+
+  return (
+    <div>
+      
+      <video autoPlay muted playsInline ref={localVideoRef} width={400}/>
+      <video autoPlay muted playsInline ref={remoteVideoRef} />
+    </div>
+  );
 }
+
+export default CallScreen;
+
