76e017b21f8780c574f37f4784141ee49b470dd2
Aruna <arunadmap@icloud.com>
2023-05-01 21:30:34 +0530

    webrtc

--- src/pages/home/webrtc/webrtc.jsx
+++ src/pages/home/webrtc/webrtc.jsx
@@ -1,111 +1,63 @@
-import { Button } from 'antd';
 import React, { useState, useEffect, useRef } from 'react';
 import io from 'socket.io-client';
 
-const socket = io('https://a29e-112-134-212-92.ngrok-free.app');
+const socket = io("http://localhost:8000");
 
-const config = {
-  iceServers: [
-    { urls: 'stun:stun.l.google.com:19302' },
-    { urls: 'stun:stun1.l.google.com:19302' },
-    { urls: 'stun:stun2.l.google.com:19302' },
-    { urls: 'stun:stun3.l.google.com:19302' },
-    { urls: 'stun:stun4.l.google.com:19302' },
-  ],
-};
-
-const App = () => {
-  const [peerConnection, setPeerConnection] = useState(null);
-  const [localStream, setLocalStream] = useState(null);
-  const [remoteStream, setRemoteStream] = useState(null);
-  const localVideoRef = useRef();
-  const remoteVideoRef = useRef();
-
-
+function App() {
+  const [videoUrl, setVideoUrl] = useState(null);
+  const videoRef = useRef(null);
 
   useEffect(() => {
-    navigator.mediaDevices.getUserMedia({ video: true, audio: false })
-      .then(stream => {
-        setLocalStream(stream);
-        localVideoRef.current.srcObject = stream;
+    // get access to the user's webcam
+    navigator.mediaDevices.getUserMedia({ video: true })
+      .then((stream) => {
+        // create a video element to display the webcam feed
+        const video = videoRef.current;
+        video.srcObject = stream;
+        video.play();
+        console.log("stream", stream);
+        // send the video stream to the server through socket.io
+        const sendStream = () => {
+          const canvas = document.createElement('canvas');
+          canvas.width = video.videoWidth;
+          canvas.height = video.videoHeight;
+          const context = canvas.getContext('2d');
+          context.drawImage(video, 0, 0, canvas.width, canvas.height);
+          const dataUrl = canvas.toDataURL('image/jpeg', 0.5);
+          console.log("sendingStream");
+          socket.emit('stream', dataUrl);
+          requestAnimationFrame(sendStream);
+        };
+        sendStream();
       })
-      .catch(error => console.log('Error accessing media devices: ', error));
+      .catch((error) => {
+        console.error(error);
+      });
+
+    // listen for incoming video streams from the server
+    socket.on('stream', (dataUrl) => {
+      console.log("data url receiving");
+      setVideoUrl(dataUrl);
+    });
+
+    // cleanup function to stop streaming on unmount
+    return () => {
+      socket.off('stream');
+    };
   }, []);
 
-  useEffect(() => {
-    if (peerConnection) {
-      peerConnection.onicecandidate = event => {
-        if (event.candidate) {
-          console.log('on ice candidate event', event);
-          socket.emit('iceCandidate', event.candidate);
-        }
-      };
-
-      peerConnection.ontrack = event => {
-        console.log("ontrack event", event.streams[0]);
-        console.log("local Stream" , localStream);
-        setRemoteStream(event.streams[0]);
-        remoteVideoRef.current.srcObject = event.streams[0];
-      };
-
-      socket.on('iceCandidate', candidate => {
-        console.log("new iceCandidate");
-        peerConnection.addIceCandidate(new RTCIceCandidate(candidate));
-      });
-
-      socket.on('sdp', async sdp => {
-        
-        if (sdp.type === 'offer') {
-          console.log('sdp offer', sdp);
-          await peerConnection.setRemoteDescription(new RTCSessionDescription(sdp));
-          const answer = await peerConnection.createAnswer();
-          await peerConnection.setLocalDescription(answer);
-          console.log('sending sdp answer ', answer);
-          socket.emit('sdp', answer);
-        } else if (sdp.type === 'answer') {
-          console.log('sdp answer', sdp);
-          await peerConnection.setRemoteDescription(new RTCSessionDescription(sdp));
-        }
-      });
-    }
-  }, [peerConnection]);
-
-  const createPeerConnection = () => {
-    console.log("hello");
-    const pc = new RTCPeerConnection(config);
-    localStream.getTracks().forEach(track => pc.addTrack(track, localStream));
-    setPeerConnection(pc);
-  };
-
-  const initiateCall = async () => {
-    if (localStream) {
-      console.log("hello");
-    const pc = new RTCPeerConnection(config);
-    localStream.getTracks().forEach(track => pc.addTrack(track, localStream));
-    
-      const offer = await pc.createOffer();
-      await pc.setLocalDescription(offer);
-      setPeerConnection(pc);
-      console.log("offer", offer);
-      socket.emit('sdp', offer);
-    }
-  };
-
-
-  
-    
-
-
-
   return (
-    <div style={{position:'relative'}}>
-      
-      <video ref={remoteVideoRef} autoPlay width={500} style={{position:'absolute'}}/>
-      <video ref={localVideoRef} autoPlay muted width={300} style={{position:'absolute'}}/>
-      
-      <Button onClick={initiateCall}>Start Call</Button>
+    <div>
+      <h1>Webcam Stream</h1>
+      <video ref={videoRef} width="640" height="480" muted></video>
+      {videoUrl && (
+        <div>
+          <h1>Broadcasted Stream</h1>
+          <video src={videoUrl} width="640" height="480" controls></video>
+        </div>
+      )}
     </div>
   );
-};
+}
 
 export default App;
